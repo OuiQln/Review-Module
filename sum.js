8d3900eb41765ff5f0d492763a3589b4cdb285b5
Aqil Thanawala <aqil.thanawala@gmail.com>
2019-02-09 09:54:35 -0800

    Add dividend test that passes

--- sum.js
+++ sum.js
@@ -1,10 +1,13 @@
-//  added 35 to test if Travis will prevent a merge request with a failing test
+//  added dividend with test
 
 const sum = (a, b) => a + b;
 
 const prod = (a, b) => a * b;
 
+const dividend = (a, b) => a / b;
+
 module.exports = {
   sum,
   prod,
+  dividend,
 };
--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,4 @@
-const { sum, prod } = require('./sum.js');
+const { sum, prod, dividend } = require('./sum.js');
 
 test('adds 2 and 3 to get 5', () => {
   expect(sum(2, 3)).toBe(5);
@@ -6,4 +6,8 @@ test('adds 2 and 3 to get 5', () => {
 
 test('multiplies 2 and 3 to get 6', () => {
   expect(prod(2, 3)).toBe(6);
+});
+
+test('divides 10 and 5 to get 2', () => {
+  expect(dividend(10, 5)).toBe(2);
 });
